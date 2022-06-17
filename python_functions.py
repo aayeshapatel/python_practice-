1e6326cc870f872d945e63b9ff9407c859ad1f03
aayeshapatel <itsayesha313@gmail.com>
2022-06-17 17:28:29 +0530

    added 3 new lm model code

--- python_functions.py
+++ python_functions.py
@@ -2,4 +2,13 @@ python_functions.py
 
 print("adding  all functions here " )
 
+def model1():
+    print("adding code for ML model 1") 
 
+
+def model2():
+    print("adding code for ML model 2") 
+
+
+def model3():
+    print("adding code for ML model 3") 
